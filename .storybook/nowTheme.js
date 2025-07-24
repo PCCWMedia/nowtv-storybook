73c224d448ff4422ed367da0a41cd06f63790822
PCCWMedia <pccwmediahk@gmail.com>
2025-07-24 12:59:43 +0800

    Delete .storybook/nowTheme.js

--- .storybook/nowTheme.js
+++
@@ -1,9 +0,0 @@
-import { create } from 'storybook/theming';
- 
-export default create({
-  base: 'dark',
-  brandTitle: 'Now TV Storybook',
-  brandUrl: 'https://www.nowtv.now.com',
-  brandImage: '/nowtv-logo-w.svg',
-  brandTarget: '_self',
-});
