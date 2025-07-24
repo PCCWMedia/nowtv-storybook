6afba8d8219213d49da5443cfb1bec6164eabbaf
Your Name <your.email@example.com>
2025-07-24 12:51:42 +0800

    Update Storybook brand logo to nowtv-logo-w.svg

--- .storybook/nowTheme.js
+++ .storybook/nowTheme.js
@@ -4,6 +4,6 @@ export default create({
   base: 'dark',
   brandTitle: 'Now TV Storybook',
   brandUrl: 'https://www.nowtv.now.com',
-  brandImage: '/nowtv-logo.svg',
+  brandImage: '/nowtv-logo-w.svg',
   brandTarget: '_self',
 });
