57b7138e347e1bf8dcf3c832c51071b9429f1506
Your Name <your.email@example.com>
2025-07-31 10:26:25 +0800

    Updates

---
+++ assets/DevicesApp.stories-C5wWihCb.js
@@ -0,0 +1,19 @@
+import"./iframe-DqpSBh7L.js";import{D as n,w as t,i as o,g as m,n as s}from"./wm-nowplayer-web-2-CH2W3-N3.js";import"./jsx-runtime-D_zvdyIk.js";const l={title:"Components/Support/DevicesApp",component:n,parameters:{},tags:["autodocs"]},e={args:{mainImage:s,mainImageAlt:"Devices",appIcons:[{href:"#",imgSrc:t,alt:"App 3"},{href:"#",imgSrc:o,alt:"App 1"},{href:"#",imgSrc:m,alt:"App 2"}]}};var a,r,p;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    mainImage: deviceImage,
+    mainImageAlt: 'Devices',
+    appIcons: [{
+      href: '#',
+      imgSrc: watchWebImg,
+      alt: 'App 3'
+    }, {
+      href: '#',
+      imgSrc: appStoreImg,
+      alt: 'App 1'
+    }, {
+      href: '#',
+      imgSrc: googlePlayStoreImg,
+      alt: 'App 2'
+    }]
+  }
+}`,...(p=(r=e.parameters)==null?void 0:r.docs)==null?void 0:p.source}}};const I=["Default"];export{e as Default,I as __namedExportsOrder,l as default};
---
+++ assets/NavigationBar.stories-BcRyYqyc.js
@@ -0,0 +1,17 @@
+import{H as d}from"./NavigationBar-Bp_6PxUx.js";import"./jsx-runtime-D_zvdyIk.js";import"./iframe-DqpSBh7L.js";import"./Logo-CkPCtlB3.js";import"./nowtv-logo-5ypTsksM.js";import"./NavItemList-Cy-F0eZN.js";import"./NavItem-CXvDGvg_.js";import"./Search-Bs4XYXD8.js";import"./InputBox-BRx9wQTm.js";import"./ProfileButton-EEsBq3TR.js";const x={title:"Components/NavigationBar",component:d,parameters:{},tags:["autodocs"],argTypes:{status:{control:"select",options:["normal","subscription","logged-in"]},tabActive:{control:"boolean"}}},r={args:{status:"normal",tabActive:!1}},s={args:{status:"subscription"}},t={args:{status:"logged-in",username:"Now TV",tabActive:!0}};var e,a,o;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    status: 'normal',
+    tabActive: false
+  }
+}`,...(o=(a=r.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};var n,i,c;s.parameters={...s.parameters,docs:{...(n=s.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    status: 'subscription'
+  }
+}`,...(c=(i=s.parameters)==null?void 0:i.docs)==null?void 0:c.source}}};var m,p,u;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    status: 'logged-in',
+    // Replace with dynamic username if needed
+    username: "Now TV",
+    tabActive: true
+  }
+}`,...(u=(p=t.parameters)==null?void 0:p.docs)==null?void 0:u.source}}};const y=["Normal","Subscription","LoggedIn"];export{t as LoggedIn,r as Normal,s as Subscription,y as __namedExportsOrder,x as default};
