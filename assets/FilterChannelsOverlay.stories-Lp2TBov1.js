e873a6f3315ecb6b38fff580f245370c8f497ff2
Your Name <your.email@example.com>
2025-07-31 10:39:27 +0800

    Updates

---
+++ assets/FilterChannelsOverlay.stories-Lp2TBov1.js
@@ -0,0 +1,6 @@
+import{F as s}from"./FilterChannelsOverlay-BxHHnQyD.js";import"./jsx-runtime-D_zvdyIk.js";import"./iframe-DEQmBySG.js";import"./Button-DRLK8GQ5.js";import"./channels-D3YE89bj.js";import"./plans-ZTLoLkaF.js";const{fn:a}=__STORYBOOK_MODULE_TEST__,O={title:"Components/Overlay/FilterChannelsOverlay",component:s,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{isOpen:{control:"boolean"},onClose:{action:"closed"}}},e={args:{isOpen:!0,onClose:a()}};var o,r,t;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    isOpen: true,
+    onClose: fn()
+  }
+}`,...(t=(r=e.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const u=["Default"];export{e as Default,u as __namedExportsOrder,O as default};
---
+++ assets/Footer.stories-DneiRaP5.js
@@ -0,0 +1,9 @@
+import{F as p}from"./Footer-DoOfDfZf.js";import"./jsx-runtime-D_zvdyIk.js";import"./iframe-DEQmBySG.js";const g={title:"Components/Footer",component:p,parameters:{},tags:["autodocs"],argTypes:{status:{control:"select",options:["normal","copyrightOnly"]}}},r={args:{status:"normal"}},o={args:{status:"copyrightOnly"}};var s,t,a;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    status: 'normal'
+  }
+}`,...(a=(t=r.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};var e,n,c;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    status: 'copyrightOnly'
+  }
+}`,...(c=(n=o.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};const u=["Normal","CopyrightOnly"];export{o as CopyrightOnly,r as Normal,u as __namedExportsOrder,g as default};
