8a53fbfe950ca41bfbbb14d8fc44654fc873185f
Your Name <your.email@example.com>
2025-07-31 11:43:11 +0800

    Updates

---
+++ assets/Header.stories-Dk1XA2UT.js
@@ -0,0 +1,16 @@
+import{H as l}from"./Header-XpCwv2Do.js";import"./jsx-runtime-D_zvdyIk.js";import"./iframe-Bgd-R5Cn.js";import"./NavigationBar-iMs0wU2W.js";import"./Logo-CiYIBaNQ.js";import"./nowtv-logo-5ypTsksM.js";import"./NavItemList-W2M3gjLf.js";import"./NavItem-B3tYxVen.js";import"./Search-Cjs8uwIt.js";import"./InputBox-UB36L9PC.js";import"./ProfileButton-BjhPPoje.js";import"./MobileSideNav-RhQI2sC3.js";import"./MenuList-D0ZOUj8I.js";import"./SocialMediaLinks-CDzlnIm1.js";import"./MobileSideSearch-B6FVXU-c.js";import"./Button-CJ4CuJV6.js";const _={title:"Components/Header",component:l,parameters:{layout:"fullscreen"},tags:["autodocs"],argTypes:{status:{control:"select",options:["normal","logged-in"]},tabActive:{control:"boolean"}}},r={args:{status:"normal",tabActive:!1}},t={args:{status:"subscription"}},o={args:{status:"logged-in",username:"Now TV",tabActive:!1}};var s,e,a;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    status: 'normal',
+    tabActive: false
+  }
+}`,...(a=(e=r.parameters)==null?void 0:e.docs)==null?void 0:a.source}}};var n,i,m;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    status: 'subscription'
+  }
+}`,...(m=(i=t.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};var p,c,u;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    status: 'logged-in',
+    username: 'Now TV',
+    tabActive: false
+  }
+}`,...(u=(c=o.parameters)==null?void 0:c.docs)==null?void 0:u.source}}};const h=["Normal","Subscription","LoggedIn"];export{o as LoggedIn,r as Normal,t as Subscription,h as __namedExportsOrder,_ as default};
--- assets/MobileSideSearch.stories-DlIDgmTh.js
+++
@@ -1,6 +0,0 @@
-import{e as n}from"./iframe-DI1nDLX8.js";import{M as a}from"./MobileSideSearch-_MPUnFiM.js";import"./jsx-runtime-D_zvdyIk.js";import"./InputBox-BtAnVbIG.js";import"./Button-iXIkwWhR.js";const l={title:"Components/MobileSideSearch",component:a},e=()=>n.createElement(a,{isOpen:!0,onClose:()=>{}});e.__docgenInfo={description:"",methods:[],displayName:"Default"};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`() => {
-  return React.createElement(MobileSideSearch, {
-    isOpen: true,
-    onClose: () => {}
-  });
-}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const d=["Default"];export{e as Default,d as __namedExportsOrder,l as default};
