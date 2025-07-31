b6a5f115d3baa16a202371bbab80bb407f674f45
Your Name <your.email@example.com>
2025-07-31 12:15:29 +0800

    Updates

---
+++ assets/LikeNowEOverlay.stories-12K7MQ6j.js
@@ -0,0 +1,5 @@
+import{L as s}from"./LikeNowEOverlay-yXpBN30h.js";import"./jsx-runtime-D_zvdyIk.js";import"./iframe-DEdcsZVh.js";import"./Button-w2FXW7yK.js";import"./Link-C8V0fvPy.js";import"./nowtv-logo-5ypTsksM.js";const i={title:"components/Overlay/LikeNowEOverlay",component:s,parameters:{layout:"centered"}},e={args:{onClose:()=>console.log("Close clicked")}};var o,r,t;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    onClose: () => console.log('Close clicked')
+  }
+}`,...(t=(r=e.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const d=["Default"];export{e as Default,d as __namedExportsOrder,i as default};
