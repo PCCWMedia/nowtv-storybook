bc7f4a53c390e90ae8563d2e643fe64a2910d046
Your Name <your.email@example.com>
2025-08-15 12:35:04 +0800

    Updates

---
+++ assets/MobileSideSearch.stories-Bhz_yanB.js
@@ -0,0 +1,6 @@
+import{e as n}from"./iframe-CQoWA9A0.js";import{M as a}from"./MobileSideSearch-CHq16B0M.js";import"./jsx-runtime-D_zvdyIk.js";import"./InputBox-Bqnowkni.js";import"./Button-DyV-W0O9.js";const l={title:"Components/MobileSideSearch",component:a},e=()=>n.createElement(a,{isOpen:!0,onClose:()=>{}});e.__docgenInfo={description:"",methods:[],displayName:"Default"};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`() => {
+  return React.createElement(MobileSideSearch, {
+    isOpen: true,
+    onClose: () => {}
+  });
+}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const d=["Default"];export{e as Default,d as __namedExportsOrder,l as default};
--- assets/NavItem-C5OP4ITQ.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./jsx-runtime-D_zvdyIk.js";import"./iframe-DXBncEuS.js";const d=({href:t="#",label:s,dropdownItems:r,className:n="",buttonClass:i="",onClick:l})=>e.jsxs("li",{className:"nav-item",children:[e.jsx("a",{href:t,onClick:l,className:`${n} ${r?"has-dropdown":""} ${i}`,children:s}),r&&e.jsx("ul",{className:"dropDown",children:r.map((a,u)=>e.jsx("li",{children:e.jsx("a",{href:a.href,target:a.target,children:a.label})},u))})]});d.__docgenInfo={description:"",methods:[],displayName:"NavItem",props:{href:{required:!1,tsType:{name:"string"},description:"",defaultValue:{value:"'#'",computed:!1}},label:{required:!0,tsType:{name:"string"},description:""},dropdownItems:{required:!1,tsType:{name:"Array",elements:[{name:"signature",type:"object",raw:`{
-    href: string;
-    label: string;
-    target?: string;
-}`,signature:{properties:[{key:"href",value:{name:"string",required:!0}},{key:"label",value:{name:"string",required:!0}},{key:"target",value:{name:"string",required:!1}}]}}],raw:`Array<{
-    href: string;
-    label: string;
-    target?: string;
-}>`},description:""},className:{required:!1,tsType:{name:"string"},description:"",defaultValue:{value:"''",computed:!1}},buttonClass:{required:!1,tsType:{name:"string"},description:"",defaultValue:{value:"''",computed:!1}},onClick:{required:!1,tsType:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}}},description:""}}};export{d as N};
