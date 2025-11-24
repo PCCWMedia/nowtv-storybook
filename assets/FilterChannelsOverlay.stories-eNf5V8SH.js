8d56843b526afc9181209da8398fd619f49b88ea
Your Name <your.email@example.com>
2025-11-24 10:47:19 +0800

    Updates

--- assets/FilterChannelsOverlay.stories-eNf5V8SH.js
+++
@@ -1,6 +0,0 @@
-import{F as s}from"./FilterChannelsOverlay-BfTclSLK.js";import"./jsx-runtime-D_zvdyIk.js";import"./iframe-DelINo8x.js";import"./Button-BiEGtDng.js";import"./channels-CsYm1f0D.js";import"./plans-ZTLoLkaF.js";const{fn:a}=__STORYBOOK_MODULE_TEST__,O={title:"Components/Overlay/FilterChannelsOverlay",component:s,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{isOpen:{control:"boolean"},onClose:{action:"closed"}}},e={args:{isOpen:!0,onClose:a()}};var o,r,t;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    isOpen: true,
-    onClose: fn()
-  }
-}`,...(t=(r=e.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const u=["Default"];export{e as Default,u as __namedExportsOrder,O as default};
---
+++ assets/NavItemList.stories-be-bnGiY.js
@@ -0,0 +1,40 @@
+import{N as m}from"./NavItemList-C7oCrrIu.js";import"./jsx-runtime-D_zvdyIk.js";import"./iframe-DZVqfSMv.js";import"./NavItem-D_zlKCWO.js";const b={title:"Components/NavigationBar/DesktopNav/NavItemList",component:m,parameters:{layout:"centered"},tags:["autodocs"]},e={args:{items:[{label:"Subscribe",dropdownItems:[{href:"#",label:"New Customer"},{href:"#",label:"Existing Customer"},{href:"#",label:"Now E Customer"}]},{label:"Support",href:"#"}]}},r={args:{items:[{label:"Subscribe",className:"active",dropdownItems:[{href:"#",label:"New Customer"},{href:"#",label:"Existing Customer"},{href:"#",label:"Now E Customer"}]},{label:"Support",href:"#"}]}};var t,s,a;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    items: [{
+      label: 'Subscribe',
+      dropdownItems: [{
+        href: '#',
+        label: 'New Customer'
+      }, {
+        href: '#',
+        label: 'Existing Customer'
+      }, {
+        href: '#',
+        label: 'Now E Customer'
+      }]
+    }, {
+      label: 'Support',
+      href: '#'
+    }]
+  }
+}`,...(a=(s=e.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};var o,n,l;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    items: [{
+      label: 'Subscribe',
+      className: 'active',
+      dropdownItems: [{
+        href: '#',
+        label: 'New Customer'
+      }, {
+        href: '#',
+        label: 'Existing Customer'
+      }, {
+        href: '#',
+        label: 'Now E Customer'
+      }]
+    }, {
+      label: 'Support',
+      href: '#'
+    }]
+  }
+}`,...(l=(n=r.parameters)==null?void 0:n.docs)==null?void 0:l.source}}};const f=["Default","WithActiveItem"];export{e as Default,r as WithActiveItem,f as __namedExportsOrder,b as default};
