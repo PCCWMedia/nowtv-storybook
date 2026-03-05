1f339ccce2bc4ddca3b44fe11ac7afb318c3638b
Your Name <your.email@example.com>
2026-03-05 16:42:53 +0800

    Updates

---
+++ assets/Header.stories-BCOX3JV7.js
@@ -0,0 +1,20 @@
+import{H as f}from"./Header-CU5esuK7.js";import"./jsx-runtime-D_zvdyIk.js";import"./iframe-B2W2qgob.js";import"./NavigationBar-D8ZJbGqV.js";import"./Logo-B_KNPuD7.js";import"./nowtv-logo-5ypTsksM.js";import"./NavItemList-B5jDN5jk.js";import"./NavItem-7aO5lD00.js";import"./Search-QdwFaJXv.js";import"./InputBox-b3niqNIF.js";import"./ProfileButton-BpnBtucn.js";import"./MobileSideNav-CKY8q4Wa.js";import"./MenuList-CGx0Jaoo.js";import"./SocialMediaLinks-DJJb5wj3.js";import"./MobileSideSearch-xFn4j8pu.js";import"./Button-D8is7Onb.js";const M={title:"Components/Header",component:f,parameters:{layout:"fullscreen"},tags:["autodocs"],argTypes:{status:{control:"select",options:["normal","logged-in"]},tabActive:{control:"boolean"}}},r={args:{status:"normal",tabActive:!1}},s={args:{status:"subscription"}},o={args:{status:"logged-in",username:"Now TV",tabActive:!1}},t={args:{status:"nowid"}};var a,e,n;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    status: 'normal',
+    tabActive: false
+  }
+}`,...(n=(e=r.parameters)==null?void 0:e.docs)==null?void 0:n.source}}};var i,c,m;s.parameters={...s.parameters,docs:{...(i=s.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    status: 'subscription'
+  }
+}`,...(m=(c=s.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};var p,u,d;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    status: 'logged-in',
+    username: 'Now TV',
+    tabActive: false
+  }
+}`,...(d=(u=o.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var l,g,b;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    status: 'nowid'
+  }
+}`,...(b=(g=t.parameters)==null?void 0:g.docs)==null?void 0:b.source}}};const O=["Normal","Subscription","LoggedIn","nowid"];export{o as LoggedIn,r as Normal,s as Subscription,O as __namedExportsOrder,M as default,t as nowid};
---
+++ assets/NavigationBar.stories-zcuLZt1M.js
@@ -0,0 +1,21 @@
+import{H as w}from"./NavigationBar-D8ZJbGqV.js";import"./jsx-runtime-D_zvdyIk.js";import"./iframe-B2W2qgob.js";import"./Logo-B_KNPuD7.js";import"./nowtv-logo-5ypTsksM.js";import"./NavItemList-B5jDN5jk.js";import"./NavItem-7aO5lD00.js";import"./Search-QdwFaJXv.js";import"./InputBox-b3niqNIF.js";import"./ProfileButton-BpnBtucn.js";const L={title:"Components/NavigationBar",component:w,parameters:{},tags:["autodocs"],argTypes:{status:{control:"select",options:["normal","subscription","logged-in"]},tabActive:{control:"boolean"}}},r={args:{status:"normal",tabActive:!1}},s={args:{status:"subscription"}},a={args:{status:"logged-in",username:"Now TV",tabActive:!0}},e={args:{status:"nowid"}};var t,o,n;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    status: 'normal',
+    tabActive: false
+  }
+}`,...(n=(o=r.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};var c,i,m;s.parameters={...s.parameters,docs:{...(c=s.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    status: 'subscription'
+  }
+}`,...(m=(i=s.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};var p,u,d;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    status: 'logged-in',
+    // Replace with dynamic username if needed
+    username: "Now TV",
+    tabActive: true
+  }
+}`,...(d=(u=a.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var g,l,b;e.parameters={...e.parameters,docs:{...(g=e.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    status: 'nowid'
+  }
+}`,...(b=(l=e.parameters)==null?void 0:l.docs)==null?void 0:b.source}}};const V=["Normal","Subscription","LoggedIn","Nowid"];export{a as LoggedIn,r as Normal,e as Nowid,s as Subscription,V as __namedExportsOrder,L as default};
