d6b7889b00292a8b582dddb41d4ed67e732f3440
Your Name <your.email@example.com>
2026-04-23 10:31:59 +0800

    Updates

---
+++ assets/Header.stories-CjU6V_Ex.js
@@ -0,0 +1,20 @@
+import{H as f}from"./Header-B3HXRxbA.js";import"./jsx-runtime-D_zvdyIk.js";import"./iframe-DtYdNJka.js";import"./NavigationBar-MoMYAcaC.js";import"./Logo-CNPIVToi.js";import"./nowtv-logo-5ypTsksM.js";import"./NavItemList-Bk95vnto.js";import"./NavItem-DQ5gM7vy.js";import"./Search-BIwsL5zy.js";import"./InputBox-CIJSN61q.js";import"./ProfileButton-B84wdboU.js";import"./MobileSideNav-D6nfSeNj.js";import"./MenuList-M3E2kc61.js";import"./SocialMediaLinks-C3G9KCOH.js";import"./MobileSideSearch-B8pOsRMq.js";import"./Button-BZEwkvif.js";const M={title:"Components/Header",component:f,parameters:{layout:"fullscreen"},tags:["autodocs"],argTypes:{status:{control:"select",options:["normal","logged-in"]},tabActive:{control:"boolean"}}},r={args:{status:"normal",tabActive:!1}},s={args:{status:"subscription"}},o={args:{status:"logged-in",username:"Now TV",tabActive:!1}},t={args:{status:"nowid"}};var a,e,n;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    status: 'normal',
+    tabActive: false
+  }
+}`,...(n=(e=r.parameters)==null?void 0:e.docs)==null?void 0:n.source}}};var i,c,m;s.parameters={...s.parameters,docs:{...(i=s.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    status: 'subscription'
+  }
+}`,...(m=(c=s.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};var p,u,d;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    status: 'logged-in',
+    username: 'Now TV',
+    tabActive: false
+  }
+}`,...(d=(u=o.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var l,g,b;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    status: 'nowid'
+  }
+}`,...(b=(g=t.parameters)==null?void 0:g.docs)==null?void 0:b.source}}};const O=["Normal","Subscription","LoggedIn","nowid"];export{o as LoggedIn,r as Normal,s as Subscription,O as __namedExportsOrder,M as default,t as nowid};
