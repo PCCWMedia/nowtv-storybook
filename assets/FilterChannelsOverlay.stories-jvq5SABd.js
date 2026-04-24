5508091bd3a5457cea7440c9806e6c611d1adfb7
Your Name <your.email@example.com>
2026-04-24 16:21:57 +0800

    Updates

---
+++ assets/FilterChannelsOverlay.stories-jvq5SABd.js
@@ -0,0 +1,6 @@
+import{F as s}from"./FilterChannelsOverlay-D8da0aji.js";import"./jsx-runtime-D_zvdyIk.js";import"./iframe-DUt0M-Je.js";import"./Button-C1F5Oi9n.js";import"./channels-CK96P0lc.js";import"./plans-ZTLoLkaF.js";const{fn:a}=__STORYBOOK_MODULE_TEST__,O={title:"Components/Overlay/FilterChannelsOverlay",component:s,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{isOpen:{control:"boolean"},onClose:{action:"closed"}}},e={args:{isOpen:!0,onClose:a()}};var o,r,t;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    isOpen: true,
+    onClose: fn()
+  }
+}`,...(t=(r=e.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const u=["Default"];export{e as Default,u as __namedExportsOrder,O as default};
