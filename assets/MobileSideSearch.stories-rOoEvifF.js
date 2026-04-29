6203a0e04c9f6478c79c9e7ca5c6f516a69c9597
Your Name <your.email@example.com>
2026-04-29 15:17:40 +0800

    Updates

--- assets/MobileSideSearch.stories-rOoEvifF.js
+++
@@ -1,6 +0,0 @@
-import{e as n}from"./iframe-DIZmOQJh.js";import{M as a}from"./MobileSideSearch-CTHVKjPB.js";import"./jsx-runtime-D_zvdyIk.js";import"./InputBox-DcuJ8vfJ.js";import"./Button-DbVjC_Yz.js";const l={title:"Components/MobileSideSearch",component:a},e=()=>n.createElement(a,{isOpen:!0,onClose:()=>{}});e.__docgenInfo={description:"",methods:[],displayName:"Default"};var r,o,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`() => {
-  return React.createElement(MobileSideSearch, {
-    isOpen: true,
-    onClose: () => {}
-  });
-}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const d=["Default"];export{e as Default,d as __namedExportsOrder,l as default};
